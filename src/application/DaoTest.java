1af02b5c2ddf213709a948da4a0cb3c9ca28d727
Alexdevsoft <alexhavilla2022@gmail.com>
2023-02-07 08:26:03 -0300

    Seller entitie class

--- src/application/DaoTest.java
+++ src/application/DaoTest.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class DaoTest {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Livros");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Alex", "alex@gamil.com", new Date(), 5000.0, obj);
+		System.out.println(seller);
 		
 	}
 
