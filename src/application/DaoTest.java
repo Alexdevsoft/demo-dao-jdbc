c4040daf3e39b4855633917cf9aa444c6c95edb3
Alexdevsoft <alexhavilla2022@gmail.com>
2023-02-07 15:35:17 -0300

    FindAll implementation

--- src/application/DaoTest.java
+++ src/application/DaoTest.java
@@ -12,19 +12,27 @@ public class DaoTest {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TESTE 1 - SELLER BY ID ===");
 
 		Seller seller = sellerDao.findById(3);
 
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TESTE 2 - SELLER BY DEPARTMENT ID ===");
-		
+
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
-		for(Seller obj : list) {
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n=== TESTE 3 - SELLER BY DEPARTMENT FINDALL ===");
+
+		list = sellerDao.findAll();
+
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 
